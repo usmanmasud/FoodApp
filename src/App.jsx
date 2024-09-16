c77adc993ff7dd0d4ae8d3440861abd039408fbf
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-17 00:43:19 +0100

    Api called

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Search from './components/Search'
 
 function App() {
 
+  const [foodData, setFoodData] = useState([]);
+
   return (
     <div className='App'>
-      <Search />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      {foodData.map(food => <h1>{food.title}</h1>)}
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export default function Search() {
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "c7de2a6ec6694da2815572852430a37a";
+
+export default function Search({ foodData, setFoodData }) {
     const [query, setQuery] = useState('pasta');
+    useEffect(() => {
+        async function fetchFood() {
+            const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+            const data = await res.json()
+            console.log(data.results)
+            setFoodData(data.results)
+        }
+        fetchFood()
+    }, [query]);
 
     return (
         <div>
