9e35ed68702539136811e78092557f0756648590
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-09-19 17:28:44 +0100

    intructions

--- src/components/FoodDeteil.jsx
+++ src/components/FoodDeteil.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import styles from './foodDeteils.module.css'
+
 
 export default function FoodDeteil({ foodId }) {
     const [food, setFood] = useState({})
-    const [isLoading, setIsLoading] = useEffect(true);
+    const [isLoading, setIsLoading] = useState(true);
 
     const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
     const API_KEY = "a1dd02a130c94a13a7470cc12d3df4e3";
@@ -12,37 +14,38 @@ export default function FoodDeteil({ foodId }) {
             const data = await res.json()
             console.log(data)
             setFood(data)
-            IsLoading(false)
+            setIsLoading(false)
         }
         fetchFood()
     }, [foodId])
     return (
         <div>
-            <div>
-                <h1>{food.title}</h1>
-                <img src={food.image} />
-                <div>
+            <div className={styles.recipeCard}>
+                <h1 className={styles.recipeName}>{food.title}</h1>
+                <img src={food.image} className={styles.recipeImage} />
+                <div className={styles.recipeDeteils}>
                     <span>
                         <strong>⏰{food.readyInMinutes} Minutes</strong>
-                        <span>
-                            {food.vegeterian ? "🥒Vegeterian" : "🍖Non-Vegeterian"}
-                        </span>
-                        <span>
-                            🏇<strong>Servers {food.servings}</strong>
-                            <span>
-                                {food.vegan ? "🐮 Vegan" : ''}
-                            </span>
-                        </span>
+                    </span>
+                    <span>
+                        🏇<strong>Serves {food.servings}</strong>
+                    </span>
+                    <span>
+                        <strong>{food.vegeterian ? "🥒Vegeterian" : "🍖Non-Vegeterian"}</strong>
+                    </span>
+                    <span>
+                        <strong> {food.vegan ? "🐮 Vegan" : ''}</strong>
                     </span>
                 </div>
-            </div>
-            <div>
-                💲<span>{food.pricePerServing / 100} Per serving</span>
-            </div>
-            <div>
-                {isLoading ? <p>Loading...</p> : food.analyzedInsructions[0].steps.map(step => (<li>{step.step}</li>))}
+                <div>
+                    <strong>💲<span>{food.pricePerServing} Per serving</span></strong>
+                </div>
                 <h2>Instructions</h2>
-
+                <div className={styles.recipeInstructions}>
+                    <ol>
+                        {isLoading ? <p>Loading...</p> : food.analyzedInstructions[0].steps.map(step => (<li>{step.step}</li>))}
+                    </ol>
+                </div>
             </div>
         </div>
     )
